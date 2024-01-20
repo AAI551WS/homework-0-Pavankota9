53a03a5c34929501ebebc435a5f8f1598ff281f6
Pavankota9 <156982929+Pavankota9@users.noreply.github.com>
2024-01-20 15:55:47 -0500

    Update answer.py

--- answer.py
+++ answer.py
@@ -7,8 +7,8 @@ def hello_world():
 
     # Write a comment; comments are very useful to other programmers.
     #the below statement prints helloworld
-    # Write a Python statement to print Hello, World!
-       print("Hello, World!")
+    # Write a Python statement to print Hello, World
+    print("Hello, World!")
 def add(a,b):
 
     # Write simple code to add variable a and b
